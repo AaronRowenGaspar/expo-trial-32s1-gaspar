21b403b75f6a06201b17266ee9854dc2d0b36794
Aaron Rowen Gaspar <qarogaspar@tip.edu.ph>
2026-03-27 16:27:38 +0800

    march 27

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textWithNoBorder}>Aaron Gaspar</Text>
+
+      <Text style={styles.textWithHexBorder}>AARON GASPAR</Text>
+
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="Tap me!" 
+          color="green" 
+        />
+      </View>
+
+      <Text style={styles.textWithRedBorder}>aaron gaspar</Text>
     </View>
   );
 }
@@ -16,5 +27,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textWithNoBorder: {
+    margin: 16,               
+    padding: 16,              
+  },
+  textWithHexBorder: {
+    margin: 16,               
+    padding: 16,              
+    borderWidth: 2,           
+    borderColor: '#ff5733',   
+  },
+  textWithRedBorder: {
+    margin: 16,               
+    padding: 16,              
+    borderWidth: 2,           
+    borderColor: 'red',      
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
