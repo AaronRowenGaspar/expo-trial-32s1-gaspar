f4079d3ad4120b111b4645128aa55c787795221a
Aaron Rowen Gaspar <qarogaspar@tip.edu.ph>
2026-03-30 17:09:25 +0800

    DONE

--- App.js
+++ App.js
@@ -1,50 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    if (enteredGoalText.trim().length === 0) {
+      return;
+    }
+
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText
+    ]);
+    
+    setEnteredGoalText('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textWithNoBorder}>Aaron Gaspar</Text>
-
-      <Text style={styles.textWithHexBorder}>AARON GASPAR</Text>
-
-      <View style={styles.buttonContainer}>
-        <Button 
-          title="Tap me!" 
-          color="green" 
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="Your Course Goal" 
+          style={styles.textInput} 
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
         />
+        <View style={styles.buttonContainer}>
+          <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc" />
+        </View>
       </View>
 
-      <Text style={styles.textWithRedBorder}>aaron gaspar</Text>
+      <View style={styles.goalsContainer}>
+        <Text style={styles.subtitle}>Current Goals</Text>
+        {courseGoals.map((goal, index) => (
+          <View key={index} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    backgroundColor: '#f4f4f4',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#d1d1d1',
+    paddingBottom: 20,
   },
-  textWithNoBorder: {
-    margin: 16,               
-    padding: 16,              
-  },
-  textWithHexBorder: {
-    margin: 16,               
-    padding: 16,              
-    borderWidth: 2,           
-    borderColor: '#ff5733',   
-  },
-  textWithRedBorder: {
-    margin: 16,               
-    padding: 16,              
-    borderWidth: 2,           
-    borderColor: 'red',      
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#b2b2b2',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    width: '70%',
+    padding: 12,
+    fontSize: 16,
   },
   buttonContainer: {
-    marginTop: 20,
+    width: '25%',
   },
+  goalsContainer: {
+    flex: 5,
+    marginTop: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#333',
+  },
+  goalItem: {
+    marginVertical: 6,
+    padding: 12,
+    borderRadius: 10,
+    backgroundColor: '#5e0acc',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  goalText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+  }
 });
